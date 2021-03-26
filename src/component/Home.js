8e3354c66291fe495c8f80a61a7225cda5045a3a
Ashribad samal <ashribad.samal11@gmail.com>
2021-03-26 11:33:30 -0700

    Update Home.js

--- src/component/Home.js
+++ src/component/Home.js
@@ -3,16 +3,15 @@ import './Home.css'
 
 function Home() {
     return (
-        <div>
-            <header class="showcase">
-        <div class="container showcase-inner">
-          <h1>Welcome to My Gallery</h1>
-          <p>This Gallery Where you can search any Image </p>
-          <a href="/image" class="btn">Image</a>
-        </div>
-      </header>
-             
-        </div>
+       <div>
+         <header className="showcase">
+           <div className="container showcase-inner">
+             <h1>Welcome to My Gallery</h1>
+              <p>This Gallery Where you can search any Image </p>
+               <a href="/image" className="btn">Image</a>
+           </div>
+        </header>
+       </div>
     )
 }
 
